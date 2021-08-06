52cbc32d5c1f0f3b8d9b6b95b1beda797242ece0
Brady Bott <bottbrady@gmail.com>
2021-08-06 07:56:11 -0600

    saving work

--- server/index.js
+++ server/index.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const cors = require("cors");
-
 const app = express();
 
-
 app.use(cors());
+app.use(express.json());
 
-app.use(express.json()); // When we want to be able to accept JSON.
+const suggestions = [];
 
 app.get("/api/compliment", (req, res) => {
   const compliments = ["Gee, you're a smart cookie!",
@@ -21,5 +20,26 @@ app.get("/api/compliment", (req, res) => {
   res.status(200).send(randomCompliment);
   
 });
+
+app.get('/api/fortune', (req, res) => {
+  const fortuneList = ['May your shirt sleeves be shorter than your pant legs.', 'RUN!', `"Tomorrow" is often the busiest day of the week.`, 'Life is short, smile while you still have teeth.', `Help! I'm trapped in a computer!`, 'People come and go, but the right ones stay by your side.', `Failure is when you don't try.`];
+
+  let randomIndex = Math.floor(Math.random() * fortuneList.length);
+  let randomFortune = fortuneList[randomIndex];
+
+  res.status(200).send(randomFortune);
+})
+
+app.get("/api/suggestion", (req, res) => {
+  let randomIndex = Math.floor(Math.random() * suggestions.length);
+  let randomSuggestion = suggestions[randomIndex];
+
+  res.status(200).send(randomSuggestion);
+  
+});
+
+app.post('/api/suggestion', (req, res) => {
+  let newSuggestion = req.body
+})
 
 app.listen(4000, () => console.log("Server running on 4000"));
